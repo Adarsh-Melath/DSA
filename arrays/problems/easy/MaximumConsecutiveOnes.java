ae5e0033c677541415d296306b176c0c99a7eea2
Adarsh.M <adarshmelath1305@gmail.com>
2025-12-30 10:41:46 +0530

    maximum consecutive ones problem added

---
+++ arrays/problems/easy/MaximumConsecutiveOnes.java
@@ -0,0 +1,26 @@
+
+/**
+ * MaximumConsecutiveOnes
+ */
+public class MaximumConsecutiveOnes {
+  public static void main(String[] args) {
+    int[] arr = { 1, 1, 0, 1, 1, 1, 0, 1 };
+    System.out.println(bruteForceSolution(arr));
+  }
+
+  public static int bruteForceSolution(int[] arr) {
+    int count = 0;
+    int max = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == 1) {
+        count++;
+        if (max < count) {
+          max = count;
+        }
+      } else {
+        count = 0;
+      }
+    }
+    return max;
+  }
+}
