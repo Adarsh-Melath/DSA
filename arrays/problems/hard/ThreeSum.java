7fa0c45d7672b2e98ba013c9a2b50ebd1c8b7f7c
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-22 23:28:08 +0530

    three sum problem added

--- arrays/problems/hard/ThreeSum.java
+++ arrays/problems/hard/ThreeSum.java
@@ -13,6 +13,7 @@ public class ThreeSum {
 
         System.out.println(Arrays.deepToString(bruteForce(nums).toArray()));
         System.out.println(Arrays.deepToString(betterSolution(nums).toArray()));
+        System.out.println(Arrays.deepToString(optimalSolution(nums).toArray()));
     }
 
     public static List<List<Integer>> bruteForce(int[] nums) {
@@ -45,6 +46,37 @@ public class ThreeSum {
                     triplets.add(temp);
                 }
                 set.add(nums[j]);
+            }
+        }
+        return triplets;
+    }
+
+    public static List<List<Integer>> optimalSolution(int nums[]) {
+        Arrays.sort(nums);
+        List<List<Integer>> triplets = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    triplets.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                }
             }
         }
         return triplets;
