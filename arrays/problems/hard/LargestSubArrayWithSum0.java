b8bd271a677c0d32558b3ba6a68a53bccae95601
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-30 21:48:04 +0530

    better force solution for largest sub array with sum 0 added

--- arrays/problems/hard/LargestSubArrayWithSum0.java
+++ arrays/problems/hard/LargestSubArrayWithSum0.java
@@ -1,8 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class LargestSubArrayWithSum0 {
     public static void main(String[] args) {
         int[] arr = { 15, -2, 2, -8, 1, 7, 10, 23 };
 
         System.out.println(bruteForce(arr));
+        System.out.println(betterSolution(arr));
+    }
+
+    public static int betterSolution(int[] arr) {
+        int length = 0;
+        Map<Integer, Integer> prefixSum = new HashMap<>();
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum == 0) {
+                length = Math.max(length, i + 1);
+            }
+            int remaining = sum - 0;
+            if (prefixSum.containsKey(remaining)) {
+                int prefixIndex = prefixSum.get(remaining);
+                length = Math.max(length, i - prefixIndex);
+            }
+            if (!prefixSum.containsKey(sum)) {
+                prefixSum.put(sum, i);
+            }
+        }
+
+        return length;
     }
 
     public static int bruteForce(int[] arr) {
