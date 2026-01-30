dfc16020117b28c664f631b9f43d5dd108ea7835
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-30 19:55:31 +0530

    better  solution for 4 sum problem added

--- arrays/problems/hard/FourSum.java
+++ arrays/problems/hard/FourSum.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class FourSum {
     public static void main(String[] args) {
-        int[] numbers = { 7, -7, 1, 2, 14, 3 };
-        int target = 9;
+        int[] numbers = { 1, -2, 3, 5, 7, 9 };
+        int target = 7;
 
-        System.out.println(Arrays.toString(bruteForce(numbers, target).toArray()));
+        // System.out.println(Arrays.toString(bruteForce(numbers, target).toArray()));
+        System.out.println(Arrays.deepToString(betterSolution(numbers, target).toArray()));
     }
 
     public static List<Integer> bruteForce(int[] numbers, int target) {
@@ -29,5 +33,33 @@ public class FourSum {
             }
         }
         return new ArrayList<>();
+    }
+
+    public static List<List<Integer>> betterSolution(int[] numbers, int target) {
+        int n = numbers.length;
+        List<List<Integer>> elements = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                Set<Integer> set = new HashSet<>();
+                for (int k = j + 1; k < n; k++) {
+                    int fourth = target - numbers[i] - numbers[j] - numbers[k];
+                    if (set.contains(fourth)) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(numbers[i]);
+                        temp.add(numbers[j]);
+                        temp.add(numbers[k]);
+                        temp.add(fourth);
+
+                        Collections.sort(temp);
+                        elements.add(temp);
+                    }
+                    set.add(numbers[k]);
+                }
+            }
+
+        }
+
+        return elements;
     }
 }
