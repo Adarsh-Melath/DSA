12e622034303c02bfa79ae84821352312d161c6e
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-30 22:45:36 +0530

    optimal solution for count sub array with xr as k added

--- arrays/problems/hard/CountSubArrayWithXorAsK.java
+++ arrays/problems/hard/CountSubArrayWithXorAsK.java
@@ -1,8 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountSubArrayWithXorAsK {
     public static void main(String[] args) {
         int[] arr = { 4, 2, 2, 6, 4 };
         int k = 6;
         System.out.println(bruteForce(arr, k));
+        System.out.println(optimalSolution(arr, k));
+    }
+
+    public static int optimalSolution(int[] arr, int k) {
+        int count = 0;
+        int xor = 0;
+
+        Map<Integer, Integer> prefixXor = new HashMap<>();
+        prefixXor.put(0, 1);
+
+        for (int i = 0; i < arr.length; i++) {
+            xor ^= arr[i];
+            int remaining = xor ^ k;
+            if (prefixXor.containsKey(remaining)) {
+                count += prefixXor.get(remaining);
+            }
+            prefixXor.put(xor, prefixXor.getOrDefault(xor, 0) + 1);
+        }
+
+        return count;
+
     }
 
     public static int bruteForce(int[] arr, int k) {
