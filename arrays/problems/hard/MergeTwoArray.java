9712b126a8015ca3b0125c2364d8021ec8504948
Adarsh.M <adarshmelath1305@gmail.com>
2026-01-31 21:43:23 +0530

    better solution for merge two array added

--- arrays/problems/hard/MergeTwoArray.java
+++ arrays/problems/hard/MergeTwoArray.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class MergeTwoArray {
@@ -5,7 +6,36 @@ public class MergeTwoArray {
         int[] arr1 = { -5, -2, 4, 5 };
         int[] arr2 = { -3, 1, 8 };
 
-        bruteForce(arr1, arr2, arr1.length, arr2.length);
+        // bruteForce(arr1, arr2, arr1.length, arr2.length);
+        betterSolution(arr1, arr2, arr1.length, arr2.length);
+    }
+
+    public static void betterSolution(int[] arr1, int[] arr2, int m, int n)
+    {
+        int endOfFirst = arr1.length-1;
+        int startOfSecond = 0;
+
+        while(endOfFirst>=0 && startOfSecond<arr2.length)
+        {
+            if(arr1[endOfFirst]>arr2[startOfSecond])
+            {
+                int temp = arr1[endOfFirst];
+                arr1[endOfFirst] = arr2[startOfSecond];
+                arr2[startOfSecond] = temp;
+
+                endOfFirst--;
+                startOfSecond++;
+            }
+            else {
+                break;
+            }
+        }
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
     }
 
     public static void bruteForce(int[] arr1, int[] arr2, int m, int n) {
