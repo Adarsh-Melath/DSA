14d0e938b647522e02bad98f68d9503695b74034
Adarsh.M <adarshmelath1305@gmail.com>
2026-02-01 22:01:57 +0530

    optimal solution for gcd added

--- math/GCD.java
+++ math/GCD.java
@@ -5,7 +5,9 @@ public class GCD {
         int number1 = 12;
         int number2 = 18;
 
-        System.out.println("GCD of " + number1 + " and " + number2 + " is: " + gcdBruteForce(number1, number2));
+        // System.out.println("GCD of " + number1 + " and " + number2 + " is: " +
+        // gcdBruteForce(number1, number2));
+        System.out.println("GCD of " + number1 + " and " + number2 + " is: " + getOptimalSolution(number1, number2));
     }
 
     public static int gcdBruteForce(int number1, int number2) {
@@ -17,5 +19,15 @@ public class GCD {
             }
         }
         return gcd;
+    }
+
+    public static int getOptimalSolution(int number1, int number2) {
+        while (number2 !=0) {
+            int temp = number2;
+            number2 = number1 % number2;
+            number1 = temp;
+        }
+
+        return number1;
     }
 }
