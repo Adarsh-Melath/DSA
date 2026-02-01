fb78f6c4725299f540887543f3546d83bce16038
Adarsh.M <adarshmelath1305@gmail.com>
2026-02-01 18:58:53 +0530

    inverted pyramid pattern added

---
+++ patterns/InvertedPyramid.java
@@ -0,0 +1,14 @@
+public class InvertedPyramid {
+    public static void main(String[] args) {
+        int row = 5;
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= 2*row-(2*i-1); j++) {
+                System.out.print("* ");
+            }System.out.println();
+        }
+    }
+}
